df2e4214758f23249e1adb6a6fa362915c23cf3b
shankleshdivami <shanklesh@divami.com>
2023-04-27 16:11:59 +0530

    unused block added

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ app.get("/test", (req, res) => {
   console.log("inside test api");
   console.log("========");
   const test = "Hi";
+  let s = true;
+  if (true) {
+  } else {
+  }
   res.status(200).send({ message: "test" });
 });
 
