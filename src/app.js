b639361c30e720bfdbe894006755c4bb87bd6f78
shankleshdivami <shanklesh@divami.com>
2023-04-28 15:31:51 +0530

    fdf

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.get("/test", (req, res) => {
     console.log('true');
   } 
   
-                 const testArray = ['a','b','c','d'];
+ const testArray = ['a','b','c','d'];
 
   testArray.forEach(element =>{
                console.log(element)
